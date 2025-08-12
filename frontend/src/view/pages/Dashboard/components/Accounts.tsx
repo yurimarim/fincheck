ff86214cd6e1cdf1999bd428ccc9931b44e9ad6d
yurimarim <yurimarim@hotmail.com>
2025-08-11 23:13:33 -0300

    feat: accounts section and slider management

---
+++ frontend/src/view/pages/Dashboard/components/Accounts.tsx
@@ -0,0 +1,47 @@
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EyeIcon } from '../../../components/icons/EyeIcon';
+import { AccountCard } from './AccountCard';
+import { AccountsSliderNavigation } from './AccountsSliderNavigation';
+
+export function Accounts() {
+	return (
+		<div className='bg-teal-900 rounded-2xl w-full h-full px-4 py-8 md:p-10 flex flex-col'>
+			<div>
+				<span className='tracking-[-0.5px] text-white block'>Saldo total</span>
+
+				<div className='flex items-center gap-2'>
+					<strong className='text-2xl text-white tracking-[-1px]'>R$ 1000,00</strong>
+
+					<button type='button' className='w-8 h-8 flex items-center justify-center'>
+						<EyeIcon open />
+					</button>
+				</div>
+			</div>
+
+			<div className='flex-1 flex flex-col justify-end'>
+				<div>
+					<Swiper spaceBetween={16} slidesPerView={2.1}>
+						<div className='flex items-center justify-between mb-4' slot='container-start'>
+							<strong className='tracking-[-1px] text-white text-lg'>Minhas Contas</strong>
+
+							<AccountsSliderNavigation />
+						</div>
+
+						<SwiperSlide>
+							<AccountCard name='Nubank' color='#7950F2' balance={1000.23} type='CASH' />
+						</SwiperSlide>
+
+						<SwiperSlide>
+							<AccountCard name='XP' color='#333' balance={1000.23} type='INVESTMENT' />
+						</SwiperSlide>
+
+						<SwiperSlide>
+							<AccountCard name='Carteira' color='#0f0' balance={1000.23} type='CASH' />
+						</SwiperSlide>
+					</Swiper>
+				</div>
+			</div>
+		</div>
+	);
+}
