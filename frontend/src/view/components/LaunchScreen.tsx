8ce993194ab79cf238284c813f9a03b2933ca2ed
yurimarim <yurimarim@hotmail.com>
2025-08-18 19:09:03 -0300

    fix: launch screen z-index correction

--- frontend/src/view/components/LaunchScreen.tsx
+++ frontend/src/view/components/LaunchScreen.tsx
@@ -17,7 +17,7 @@ export function LaunchScreen({ isLoading }: ILaunchScreen) {
 			leaveFrom='opacity-100'
 			leaveTo='opacity-0'
 		>
-			<div className='bg-teal-900 fixed top-0 left-0 w-full h-full grid place-items-center'>
+			<div className='bg-teal-900 z-50 fixed top-0 left-0 w-full h-full grid place-items-center'>
 				<div className='flex flex-col justify-center items-center gap-4'>
 					<Logo className='h-10 text-white' />
 					<Spinner className='text-teal-900 fill-white' />
