cd4e0e59c653090e836f605a74c4ff6a81b15df4
yurimarim <yurimarim@hotmail.com>
2025-08-18 20:20:25 -0300

    feat: UserMenu dropdown implemented

--- frontend/src/view/components/UserMenu.tsx
+++ frontend/src/view/components/UserMenu.tsx
@@ -1,7 +1,27 @@
+import { ExitIcon } from '@radix-ui/react-icons';
+import { useAuth } from '../../app/hooks/useAuth';
+import { DropdownMenu } from './DropdownMenu';
+
 export function UserMenu() {
+	const { signout } = useAuth();
+
 	return (
-		<div className='bg-teal-0 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100'>
-			<span className='text-sm tracking-[-0.5px] text-teal-900 font-medium'>YU</span>
-		</div>
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger>
+				<div className='bg-teal-0 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100'>
+					<span className='text-sm tracking-[-0.5px] text-teal-900 font-medium'>YU</span>
+				</div>
+			</DropdownMenu.Trigger>
+
+			<DropdownMenu.Content className='w-32'>
+				<DropdownMenu.Item
+					className='flex items-center justify-between'
+					onSelect={signout}
+				>
+					Sair
+					<ExitIcon className='w-4 h-4' />
+				</DropdownMenu.Item>
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
 	);
 }
