0ad3a4e07033cdb7937b5dd7c4b0fae5cc99e4a2
yurimarim <yurimarim@hotmail.com>
2025-08-19 12:06:30 -0300

    feat: using sleep fn

--- frontend/src/app/services/UsersService.ts
+++ frontend/src/app/services/UsersService.ts
@@ -1,3 +1,4 @@
+import { sleep } from '../utils/sleep';
 import { httpClient } from './utils/httpClient';
 
 interface IMeResponse {
@@ -8,6 +9,7 @@ interface IMeResponse {
 class UsersService {
 	async me() {
 		const { data } = await httpClient.get<IMeResponse>('/users/me');
+		await sleep();
 
 		return data;
 	}
